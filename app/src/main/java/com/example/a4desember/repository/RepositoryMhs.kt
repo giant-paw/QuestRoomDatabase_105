ddb1e5dbba2ae8c6f0d699b70b90031f694129a7
giant-paw <giantpawbusiness@gmail.com>
2024-12-16 12:28:32 +0700

    Mengimplementasikan fungsi CRUD pada entitas mahasiswa, seperti mendapatkan data mahasiswa berdasarkan NIM, menghapus data, dan memperbarui informasi mahasiswa.

--- app/src/main/java/com/example/a4desember/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/a4desember/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.a4desember.repository
 
 import com.example.a4desember.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMahasiswa():Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 }
