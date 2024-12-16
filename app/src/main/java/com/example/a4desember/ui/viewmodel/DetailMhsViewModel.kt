77e3476ba7a81fe633b5c591a3adafe8059ad4ab
giant-paw <giantpawbusiness@gmail.com>
2024-12-17 05:06:21 +0700

    data class DetailUiState di file DetailMhsViewModel.kt untuk merepresentasikan state UI halaman detail mahasiswa. DetailUiState berfungsi mengelola kondisi UI, seperti data mahasiswa, status pemuatan, error, dan event terkait.

---
+++ app/src/main/java/com/example/a4desember/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.a4desember.ui.viewmodel
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: Boolean = false,
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
