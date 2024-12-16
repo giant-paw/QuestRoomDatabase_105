04e88645b06f9066059ed47a15188c97dea97211
giant-paw <giantpawbusiness@gmail.com>
2024-12-17 05:36:41 +0700

    UpdateMhsViewModel untuk fitur pembaruan data mahasiswa. Fungsinya memuat data berdasarkan NIM, mengelola input pengguna, dan memvalidasi form sebelum menyimpan data.

--- app/src/main/java/com/example/a4desember/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/a4desember/ui/viewmodel/DetailMhsViewModel.kt
@@ -63,7 +63,7 @@ data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
     val isError: Boolean = false,
-    val errorMessage: String = false,
+    val errorMessage: String = "",
 ) {
     val isUiEventEmpty: Boolean
         get() = detailUiEvent == MahasiswaEvent()
