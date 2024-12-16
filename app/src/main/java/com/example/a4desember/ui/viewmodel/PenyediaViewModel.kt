2dfa0af033ecd18680c4fb18d042e15e3d4a8f5a
giant-paw <giantpawbusiness@gmail.com>
2024-12-17 06:02:43 +0700

    menambahkan kode di file PenyediaViewModel, initializer untuk home, detail, dan update

--- app/src/main/java/com/example/a4desember/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a4desember/ui/viewmodel/PenyediaViewModel.kt
@@ -1,7 +1,7 @@
 package com.example.a4desember.ui.viewmodel
 
-import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -11,6 +11,26 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
                 krsApp().containerApp.repositoryMhs
             )
         }
