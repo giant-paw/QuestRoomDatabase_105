5dfc6aaec58244b5716209965f8b33b3f831f2ed
giant-paw <giantpawbusiness@gmail.com>
2024-12-16 12:35:34 +0700

    Menerapkan fungsi CRUD untuk mahasiswa dengan memanfaatkan Dao.

--- app/src/main/java/com/example/a4desember/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/a4desember/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.a4desember.repository
 
 import com.example.a4desember.data.dao.MahasiswaDao
 import com.example.a4desember.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,5 +10,18 @@ class LocalRepositoryMhs (
 
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMahasiswa(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+    override fun getMhs(nim: String): Flow<Mahasiswa>{
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+    override suspend fun updateMhs(mahasiswa: Mahasiswa){
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
