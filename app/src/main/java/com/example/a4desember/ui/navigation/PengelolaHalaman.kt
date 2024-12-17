7f79ee64538e97343af11fbcdfe801ff9adc074c
giant-paw <giantpawbusiness@gmail.com>
2024-12-17 07:53:00 +0700

    penambahan DetailMhsView.kt dan penambahan kode di PengelolaHalaman.kt dimana berfungsi sebagai navigasi untuk mengatur perpindahan halamana (Home, Insert, Detail, Update)

--- app/src/main/java/com/example/a4desember/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/a4desember/ui/navigation/PengelolaHalaman.kt
@@ -3,11 +3,16 @@ package com.example.a4desember.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.a4desember.ui.view.mahasiswa.DestinasiInsert
+import com.example.a4desember.ui.view.mahasiswa.DetailMhsView
+import com.example.a4desember.ui.view.mahasiswa.HomeMhsView
 import com.example.a4desember.ui.view.mahasiswa.InsertMhsView
+import com.example.a4desember.ui.view.mahasiswa.UpdateMhsView
 
 @Composable
 fun PengelolaHalaman(
@@ -16,11 +21,79 @@ fun PengelolaHalaman(
 ) {
     NavHost(navController = navController, startDestination = DestinasiInsert.route) {
         composable(
-            route = DestinasiInsert.route
+            route = AlamatNavigasi.DestinasiHome.route
         ) {
+            HomeMhsView(
+                onDetailClick = { nim ->
+                    navController.navigate("${AlamatNavigasi.DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable(
+            route = DestinasiInsert.route
+        ){
             InsertMhsView(
-                onBack = {},
-                onNavigate = {}
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
+            )
+        }
+
+        composable(
+            AlamatNavigasi.DestinasiDetail.routesWithArg,
+            arguments = listOf(
+                navArgument(AlamatNavigasi.DestinasiDetail.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ){
+            val nim = it.arguments?.getString(AlamatNavigasi.DestinasiDetail.NIM)
+
+            nim?.let { nim ->
+                DetailMhsView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${AlamatNavigasi.DestinasiUpdate.route}/$it")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+
+        composable(
+            AlamatNavigasi.DestinasiUpdate.routesWithArg,
+            arguments = listOf(
+                navArgument(AlamatNavigasi.DestinasiUpdate.NIM) {
+                    type = NavType.StringType
+                }
+            )
+        ){
+
+            UpdateMhsView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
             )
         }
     }
--- app/src/main/java/com/example/a4desember/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/a4desember/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,23 +1,152 @@
 package com.example.a4desember.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.a4desember.data.entity.Mahasiswa
+import com.example.a4desember.ui.costumwidget.CustomTopAppBar
+import com.example.a4desember.ui.viewmodel.DetailMhsViewModel
+import com.example.a4desember.ui.viewmodel.DetailUiState
+import com.example.a4desember.ui.viewmodel.PenyediaViewModel
+import com.example.a4desember.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun DetailMhsView(
+    modifier: Modifier = Modifier,
+    viewModel: DetailMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+){
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Detail Mahasiswa"
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    onEditClick(viewModel.detailUiState.value.detailUiEvent.nim) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailMhs(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deleteMhs()
+                onDeleteClick()
+            }
+        )
+
+    }
+}
+
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+    onDeleteClick: () -> Unit = { }
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+    when{
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // Tampilkan Loading
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMhs(
